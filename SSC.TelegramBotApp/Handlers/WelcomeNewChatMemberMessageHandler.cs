da27a6b78087894e2e6b97a1978d512c6e5b147c
Anton Chernomazov <chernomazov@pointcad1.onmicrosoft.com>
2021-05-16 01:12:36 +0300

    add welcome command

--- SSC.TelegramBotApp/Handlers/WelcomeNewChatMemberMessageHandler.cs
+++ SSC.TelegramBotApp/Handlers/WelcomeNewChatMemberMessageHandler.cs
@@ -41,6 +41,51 @@ namespace SSC.TelegramBotApp.Handlers
                     parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown, 
                     replyMarkup: keyboard);
             }
+            else if(msg != null && msg.Text.IndexOf("/welcome")>=0)
+            {
+                if(msg.Entities is null)
+                {
+                    client.SendTextMessageAsync(msg.Chat.Id, "Для выполнения команды необходимо указать пользователей чата.", replyToMessageId: msg.MessageId);
+                }
+                else
+                {
+                    var mentions = "";
+
+                    for (int i = 1; i < msg.Entities.Length; i++)
+                    {
+                        var user = msg.Entities[i].User;
+                        if(user == null)
+                        {
+                            var username = msg.EntityValues.ElementAt(i).Replace("@", "");
+                            var userInfo = BotDbContext.Get().UserInfoes.FirstOrDefault(u => u.Username.Equals(username));
+                            if (userInfo != null)
+                            {
+                                var chatMember = client.GetChatMemberAsync(msg.Chat.Id, userInfo.TelegramId).Result;
+                                user = chatMember.User;
+                            }
+                        }
+
+                        if (i> 0)
+                            mentions += ", ";
+                        mentions += "[" + user.FirstName + "](tg://user?id=" + user.Id + ")";
+
+                        user.BanInChat(client, msg.Chat.Id);
+
+                        BotDbContext.Get().GetUserInfo(user); // добавить информацию о юзере в базу
+                    }
+
+                    var button = new InlineKeyboardButton();
+                    button.Text = "Принять";
+                    button.CallbackData = Bot.ACCEPT_AGREEMENT_CALLBACK;
+
+                    var keyboard = new InlineKeyboardMarkup(button);
+                    string agreement = WebConfigurationManager.AppSettings.Get("MemberAgreement");
+                    client.SendTextMessageAsync(msg.Chat.Id, $"{mentions}\n{agreement}",
+                        parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown,
+                        replyMarkup: keyboard);
+                }
+               
+            }
 
             base.Handle(client, msg);
         }

a57ed886236688c5e8553f9e6bc67a194344e93e
Romero Alfa <romero_alfa@rambler.ru>
2021-08-28 23:09:32 +0300

    fux url

--- SSC.TelegramBotApp/Models/AppSettings.cs
+++ SSC.TelegramBotApp/Models/AppSettings.cs
@@ -12,7 +12,7 @@ namespace SSC.TelegramBotApp.Models
         //public static string Url { get; set; } = @"https://9b00004ad418.ngrok.io/{0}";
         //public static string Url { get; set; } = @"https://ssctelegrambotapp20210506235851.azurewebsites.net/{0}";
 #else
-        public static string Url { get; set; } = @"https://ssctelegrambotapp20210506235851.azurewebsites.net/{0}";
+        public static string Url { get; set; } = @"https://1afa-109-252-75-4.ngrok.io/{0}";
 #endif
         public static string Name { get; set; } = "anton_cher93_bot";
         public static string Key { get; set; } = "1728956369:AAFDn4tD1ZFiCO1INKqO9dGqeVmhV1Ldj9g";
